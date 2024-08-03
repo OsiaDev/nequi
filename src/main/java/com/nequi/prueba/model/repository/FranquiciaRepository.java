4c1bee010a5d4ecdeb055aad6e9f206ac647566f
Cristian Osia <osia.developer@gmail.com>
2024-08-02 22:38:41 -0500

    Creacion del model de franquicias

--- src/main/java/com/nequi/prueba/model/repository/FranquiciaRepository.java
+++ src/main/java/com/nequi/prueba/model/repository/FranquiciaRepository.java
@@ -1,2 +1,9 @@
-package com.nequi.prueba.model.repository;public interface FranquiciaRepository {
+package com.nequi.prueba.model.repository;
+
+import com.nequi.prueba.model.entity.FranquiciaEntity;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+import java.util.UUID;
+
+public interface FranquiciaRepository extends ReactiveCrudRepository<FranquiciaEntity, UUID> {
 }
