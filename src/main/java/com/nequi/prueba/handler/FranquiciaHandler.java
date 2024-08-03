15243bb984350859da8bedb004d7f428f8deac4b
Cristian Osia <osia.developer@gmail.com>
2024-08-02 23:47:33 -0500

    reacion de anotacion handler

---
+++ src/main/java/com/nequi/prueba/handler/FranquiciaHandler.java
@@ -0,0 +1,9 @@
+package com.nequi.prueba.handler;
+
+import com.nequi.prueba.common.annotation.RestHandler;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RestHandler
+public class FranquiciaHandler {
+}
