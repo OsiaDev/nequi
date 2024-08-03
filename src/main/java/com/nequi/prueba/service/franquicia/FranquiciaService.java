62130fdf7661f67e5c4d22e2d508b40943380d65
Cristian Osia <osia.developer@gmail.com>
2024-08-02 22:39:25 -0500

    reacion de service de Franquicias

--- src/main/java/com/nequi/prueba/service/franquicia/FranquiciaService.java
+++ src/main/java/com/nequi/prueba/service/franquicia/FranquiciaService.java
@@ -1,2 +1,21 @@
-package com.nequi.prueba.service;public interface FranquiciaService {
+package com.nequi.prueba.service.franquicia;
+
+import com.nequi.prueba.model.entity.FranquiciaEntity;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface FranquiciaService {
+
+    Mono<FranquiciaEntity> save(FranquiciaEntity franquicia);
+
+    Flux<FranquiciaEntity> findAll();
+
+    Mono<FranquiciaEntity> findById(UUID uuidFranquicia);
+
+    Mono<FranquiciaEntity> update(UUID uuidFranquicia, FranquiciaEntity franquicia);
+
+    Mono<Void> deleteById(UUID uuidFranquicia);
+
 }
--- src/main/java/com/nequi/prueba/service/franquicia/FranquiciaServiceImpl.java
+++ src/main/java/com/nequi/prueba/service/franquicia/FranquiciaServiceImpl.java
@@ -1,2 +1,46 @@
-package com.nequi.prueba.service.franquicia;public class FranquiciaServiceImpl {
+package com.nequi.prueba.service.franquicia;
+
+import com.nequi.prueba.model.entity.FranquiciaEntity;
+import com.nequi.prueba.model.repository.FranquiciaRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class FranquiciaServiceImpl implements FranquiciaService {
+
+    private FranquiciaRepository repository;
+
+    @Override
+    public Mono<FranquiciaEntity> save(FranquiciaEntity franquicia) {
+        return null;
+    }
+
+    @Override
+    public Flux<FranquiciaEntity> findAll() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Mono<FranquiciaEntity> findById(UUID uuidFranquicia) {
+        return this.repository.findById(uuidFranquicia);
+    }
+
+    @Override
+    public Mono<FranquiciaEntity> update(UUID uuidFranquicia, FranquiciaEntity franquicia) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> deleteById(UUID uuidFranquicia) {
+        return this.repository.deleteById(uuidFranquicia);
+    }
+
+
 }
