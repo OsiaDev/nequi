e73062ae4ca551682d0a6a051ada76b9e922e017
Cristian Osia <osia.developer@gmail.com>
2024-08-04 03:55:38 -0500

    Handler y router para Productos

---
+++ src/main/java/com/nequi/prueba/handler/ProductoHandler.java
@@ -0,0 +1,59 @@
+package com.nequi.prueba.handler;
+
+import com.nequi.prueba.common.annotation.RestHandler;
+import com.nequi.prueba.common.validation.ObjectValidator;
+import com.nequi.prueba.model.dto.ProductoDTO;
+import com.nequi.prueba.model.entity.ProductoEntity;
+import com.nequi.prueba.service.producto.ProductoService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Log4j2
+@RestHandler
+@AllArgsConstructor
+public class ProductoHandler {
+
+    private final ProductoService service;
+
+    private final ObjectValidator validator;
+
+    private static final String ID = "idProducto";
+
+    public Mono<ServerResponse> getAll(ServerRequest request) {
+        Flux<ProductoEntity> productos = this.service.findAll();
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(productos, ProductoEntity.class);
+    }
+
+    public Mono<ServerResponse> getById(ServerRequest request) {
+        Long idProducto = Long.valueOf(request.pathVariable(ID));
+        Mono<ProductoEntity> producto = this.service.findById(idProducto);
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(producto, ProductoEntity.class);
+    }
+
+    public Mono<ServerResponse> save(ServerRequest request) {
+        Mono<ProductoDTO> productoDTO = request.bodyToMono(ProductoDTO.class).doOnNext(this.validator::validate);
+        return productoDTO.flatMap(p -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(this.service.save(p), ProductoEntity.class));
+    }
+
+    public Mono<ServerResponse> update(ServerRequest request) {
+        Long idProducto = Long.valueOf(request.pathVariable(ID));
+        Mono<ProductoDTO> productoDTO = request.bodyToMono(ProductoDTO.class).doOnNext(this.validator::validate);
+        return productoDTO.flatMap(f -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(this.service.update(idProducto, f), ProductoEntity.class));
+    }
+
+    public Mono<ServerResponse> delete(ServerRequest request) {
+        Long idProducto = Long.valueOf(request.pathVariable(ID));
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(this.service.deleteById(idProducto), ProductoEntity.class);
+    }
+
+}
---
+++ src/main/java/com/nequi/prueba/router/ProductoRouter.java
@@ -0,0 +1,25 @@
+package com.nequi.prueba.router;
+
+import com.nequi.prueba.handler.ProductoHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+@Configuration
+public class ProductoRouter {
+
+    private static final String PATH = "productos";
+
+    @Bean
+    RouterFunction<ServerResponse> routerFunction(ProductoHandler productoHandler) {
+        return RouterFunctions.route()
+                .GET(PATH + "/getAll", productoHandler::getAll)
+                .GET(PATH + "/getById/{idProducto}", productoHandler::getById)
+                .POST(PATH + "/create", productoHandler::save)
+                .PUT(PATH + "/update/{idProducto}", productoHandler::update)
+                .DELETE(PATH + "/deleteById/{idProducto}", productoHandler::delete).build();
+    }
+
+}
